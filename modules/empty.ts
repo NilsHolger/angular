acd4fb2e27ea5fa5fae1292282600ea888cefcce
Nils <nils@nilsnaegele.com>
2016-12-19 08:06:57 +0100

    Update empty.ts

--- modules/empty.ts
+++ modules/empty.ts
@@ -1,12 +1,12 @@
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright 2016 Google Inc. All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
 
-// This file intentionally left blank. It is used to load nothing in some cases.
+// This file is intentionally left blank. It is used to load nothing in some cases.
 // Such as parse5/index is redirected here instead of loading into browser.
 
 export var __empty__: any;

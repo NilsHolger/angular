46155ab191f6c60f82da5486322099dce3d158ee
Nils <nils@nilsnaegele.com>
2016-12-19 01:47:55 +0100

    Update index.ts

--- modules/playground/src/person_management/index.ts
+++ modules/playground/src/person_management/index.ts
@@ -1,100 +1,62 @@
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright 2016 Google Inc. All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
 
 import {Component, Injectable, NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-/**
- * You can find the Angular 1 implementation of this example here:
- * https://github.com/wardbell/ng1DataBinding
- */
 
-// ---- model
-
-let _nextId = 1;
-class Person {
-  personId: number;
-  mom: Person;
-  dad: Person;
-  friends: Person[];
-
-  constructor(public firstName: string, public lastName: string, public yearOfBirth: number) {
-    this.personId = _nextId++;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.mom = null;
-    this.dad = null;
-    this.friends = [];
-    this.personId = _nextId++;
-  }
-
-  get age(): number { return 2015 - this.yearOfBirth; }
-  get fullName(): string { return `${this.firstName} ${this.lastName}`; }
-  get friendNames(): string { return this.friends.map(f => f.fullName).join(', '); }
-}
-
-
-
-// ---- services
+// services
 
 @Injectable()
-class DataService {
+export class DataService {
   currentPerson: Person;
   persons: Person[];
 
   constructor() {
     this.persons = [
-      new Person('Victor', 'Savkin', 1930), new Person('Igor', 'Minar', 1920),
-      new Person('John', 'Papa', 1910), new Person('Nancy', 'Duarte', 1910),
-      new Person('Jack', 'Papa', 1910), new Person('Jill', 'Papa', 1910),
-      new Person('Ward', 'Bell', 1910), new Person('Robert', 'Bell', 1910),
-      new Person('Tracy', 'Ward', 1910), new Person('Dan', 'Wahlin', 1910)
+      new Person('Victor', 'Savkin', 1980), new Person('Igor', 'Minar', 1981),
+      new Person('Victor', 'Berchet', 1982), new Person('Vamsi', 'Varikuti', 1983),
+      new Person('Timothy', 'Blasi', 1984), new Person('Tobias', 'Bosch', 1985),
+      new Person('Miško', 'Hevery', 1986), new Person('Kara', 'Puppylove', 1987),
+      new Person('Flavio', 'Corpa', 1988), new Person('Brian', 'Ford', 1989),
+      new Person('Alex', 'Rickabaugh', 1990), new Person('João', 'Dias', 1991),
+      new Person('Alex', 'Eagle', 1992), new Person('Nils', 'Naegele', 1993)
     ];
-
-    this.persons[0].friends = [0, 1, 2, 6, 9].map(_ => this.persons[_]);
-    this.persons[1].friends = [0, 2, 6, 9].map(_ => this.persons[_]);
-    this.persons[2].friends = [0, 1, 6, 9].map(_ => this.persons[_]);
-    this.persons[6].friends = [0, 1, 2, 9].map(_ => this.persons[_]);
-    this.persons[9].friends = [0, 1, 2, 6].map(_ => this.persons[_]);
-
-    this.persons[2].mom = this.persons[5];
-    this.persons[2].dad = this.persons[4];
-    this.persons[6].mom = this.persons[8];
-    this.persons[6].dad = this.persons[7];
-
+    for (let i = 0; i < this.persons.length; i++){
+      if (this.persons[i] !== this.persons[i].friends[i]){
+        this.persons[i].friends = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(_ => this.persons[_]);
+        continue;
+      }
+    }
     this.currentPerson = this.persons[0];
   }
 }
 
-
-
-// ---- components
+// components
 
 @Component({
   selector: 'full-name-cmp',
   template: `
     <h1>Edit Full Name</h1>
     <div>
-      <form>
+      <form ngNoForm>
           <div>
             <label>
               First: <input [(ngModel)]="person.firstName" type="text" placeholder="First name">
             </label>
           </div>
-
           <div>
             <label>
               Last: <input [(ngModel)]="person.lastName" type="text" placeholder="Last name">
             </label>
           </div>
-
           <div>
             <label>{{person.fullName}}</label>
           </div>
@@ -102,45 +64,27 @@ class DataService {
     </div>
   `
 })
-class FullNameComponent {
-  constructor(private _service: DataService) {}
-  get person(): Person { return this._service.currentPerson; }
+export class FullNameComponent {
+  constructor(private dataService: DataService) {}
+  get person(): Person { return this.dataService.currentPerson; }
 }
 
 @Component({
-  selector: 'person-detail-cmp',
+  selector: 'persons-detail-cmp',
   template: `
     <h2>{{person.fullName}}</h2>
-
     <div>
-      <form>
+      <form ngNoForm>
         <div>
 					<label>First: <input [(ngModel)]="person.firstName" type="text" placeholder="First name"></label>
 				</div>
-
         <div>
 					<label>Last: <input [(ngModel)]="person.lastName" type="text" placeholder="Last name"></label>
 				</div>
-
         <div>
 					<label>Year of birth: <input [(ngModel)]="person.yearOfBirth" type="number" placeholder="Year of birth"></label>
           Age: {{person.age}}
-				</div>\
-
-        <div *ngIf="person.mom != null">
-					<label>Mom:</label>
-          <input [(ngModel)]="person.mom.firstName" type="text" placeholder="Mom's first name">
-          <input [(ngModel)]="person.mom.lastName" type="text" placeholder="Mom's last name">
-          {{person.mom.fullName}}
 				</div>
-
-        <div *ngIf="person.dad != null">
-					<label>Dad:</label>
-          <input [(ngModel)]="person.dad.firstName" type="text" placeholder="Dad's first name">
-          <input [(ngModel)]="person.dad.lastName" type="text" placeholder="Dad's last name">
-          {{person.dad.fullName}}
-				</div>
-
         <div *ngIf="person.friends.length > 0">
 					<label>Friends:</label>
           {{person.friendNames}}
@@ -149,9 +93,9 @@ class FullNameComponent {
     </div>
   `
 })
-class PersonsDetailComponent {
-  constructor(private _service: DataService) {}
-  get person(): Person { return this._service.currentPerson; }
+export class PersonsDetailComponent {
+  constructor(private dataService: DataService) {}
+  get person(): Person { return this.dataService.currentPerson; }
 }
 
 @Component({
@@ -164,17 +108,14 @@ class PersonsDetailComponent {
   			  <label (click)="select(person)">{{person.fullName}}</label>
   			</li>
   	 </ul>
-
-     <person-detail-cmp></person-detail-cmp>
+     <persons-detail-cmp></persons-detail-cmp>
     </div>
   `
 })
-class PersonsComponent {
+export class PersonsComponent {
   persons: Person[];
-
-  constructor(private _service: DataService) { this.persons = _service.persons; }
-
-  select(person: Person): void { this._service.currentPerson = person; }
+  constructor(private dataService: DataService) { this.persons = dataService.persons; }
+  select(person: Person): void { this.dataService.currentPerson = person; }
 }
 
 
@@ -184,27 +125,48 @@ class PersonsComponent {
   template: `
     <button (click)="switchToEditName()">Edit Full Name</button>
     <button (click)="switchToPersonList()">Person Array</button>
-
-    <full-name-cmp *ngIf="mode == 'editName'"></full-name-cmp>
-    <persons-cmp *ngIf="mode == 'personList'"></persons-cmp>
+    <full-name-cmp *ngIf="mode === 'editName'"></full-name-cmp>
+    <persons-cmp *ngIf="mode === 'personList'"></persons-cmp>
   `
 })
-class PersonManagementApplication {
+export class PersonManagementApplication {
   mode: string;
-
   switchToEditName(): void { this.mode = 'editName'; }
   switchToPersonList(): void { this.mode = 'personList'; }
 }
 
-@NgModule({
-  bootstrap: [PersonManagementApplication],
-  declarations:
-      [PersonManagementApplication, FullNameComponent, PersonsComponent, PersonsDetailComponent],
-  imports: [BrowserModule, FormsModule]
-})
-class ExampleModule {
+
+// model
+
+let id = 0;
+export class Person {
+  personId: number;
+  friends: Person[];
+
+  constructor(public firstName: string, public lastName: string, public yearOfBirth: number) {
+    this.personId = id++;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.friends = [];
+    console.log(id);
+  }
+
+  get age(): number { return 2016 - this.yearOfBirth; }
+  get fullName(): string { return `${this.firstName} ${this.lastName}`; }
+  get friendNames(): string { return this.friends.map(f => f.fullName).join(', '); }
 }
 
-export function main() {
-  platformBrowserDynamic().bootstrapModule(ExampleModule);
-}
+@NgModule({
+   bootstrap: [PersonManagementApplication],
+   declarations:
+       [PersonManagementApplication, FullNameComponent, PersonsComponent, PersonsDetailComponent],
+   imports: [BrowserModule, FormsModule]
+ })
+ class ExampleModule {
+ }
+
+ export function main() {
+   platformBrowserDynamic().bootstrapModule(ExampleModule);
+ }
+
+
